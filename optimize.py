217719156b06e22836d3e2a4073806e246870a69
kilojoules <jaq8@humboldt.edu>
2017-10-29 20:10:15 -0600

    everything works but I have too many design variables

--- optimize.py
+++ optimize.py
@@ -52,77 +52,87 @@ def work(DV=[265., 1., 1., 1., 1., .25, 1., 2.], t3a=293.15, ql=100, t1a=275., c
    tlo = DV[0]
    mdotHElow = DV[1]
    mdot = DV[2]
-   LHEd = DV[3]
-   HHEd = DV[4]
-   HPd = DV[5]
-   LPd = DV[6]
-   r = DV[7]
+   HPd = DV[3]
+   LPd = DV[4]
+   r = DV[5]
 
    k = 1.4
-   if LPd < 0 or HPd < 0 or LHEd < 0 or HHEd < 0: return 9999
 
    # I have a lot of statements here to catch bad DV values.
    # I would like to explicitly add these things as constraints later, though
    # some things (like negative pressure) can only be caught during the calculations.
    t4b = t1a - ql/cp/mdot
-   if mdot < .2 or t4b < 0: return 9999
    p4b = p1a * (t4b / t1a) **(k/(k-1))
-   if p4b < 50: return 9999
    p4a = p4b + h_major(L_4, LPd, mdot) * gamma
-   if p4a < 50: return 9999
    t4a = t4b * (p4a / p4b) ** ((k-1)/k)
-   if t4a < 0: return 9999
    p1b = p1a - h_major(L_1, LPd, mdot) * gamma 
-   if p1b < 50: return 99999
-   if r< 1: return 99999
-   if HPd > 10: return 9999
    t1b = t1a * (p1b / p1a) ** ((k-1)/k)
-   if t1b < 0: return 9999
    p2a = p1b * (r)
-   if p2a < 0: return 9999
    t2a = t1b * (p2a / p1b) ** ((k-1)/k)
-   if t2a < 0: return 9999
    if t2a < t1b: print 't2a < t1b: ', t2a, t1b, p2a, p1b ; quit()
    p2b = p2a - h_major(L_2, HPd, mdot) * gamma
-   if p2b < 50: return 9999
    w12 = cp*(t2a - t1b) * mdot / n_s_comp # kJ
-   if w12 < 0: print 'w12<0: ', t2a, t1b, t1a, mdot, n_s_comp, cp ; quit()
-   if p2b < 50: return 999999
    t2b = t2a * (p2b/p2a) ** ((k-1)/k)
-   if t2b < 0: return 999999
    qh = cp * (t2b - t3a) * mdot
    p3a = p2b * (t3a / t2b) ** (k/(k-1))
-   if p3a < 50: return 999999
    p3b = p3a -  h_major(L_3, HPd, mdot) * gamma
-   if p3b < 50: return 999999
    t3b = t3a * (p3b / p3a) ** ((k-1)/k)
-   if t3b < 0: return 999999
    w34 = cp * (t3b - t4a) * mdot
    mdotHEhigh = qh / (cp * (tho - t3a))
 
    dph = 100 * (tho / t3a) ** k - 100
 
-   if p4a / p3b > r: return 99999
 
    vh = (spec_v(t3a, 100) + spec_v(tho, 100)) / 2
-   wh = FANCURVE(vh * mdotHEhigh, dph)  * 1 / 0.7457 # HP to kW
+   wh = FANCURVE(vh * mdotHEhigh / 1000, dph)  * 1 / 0.7457 # HP to kW
    if wh< 0: print '*** ', dph, vh, wh ; quit()
 
 #   wh = mdotHEhigh * cp * (tho - t3a) - qh
-   if mdotHEhigh < 0.2: return 9999
-   if mdotHElow < 0.2: return 9999
    #wl = mdotHElow * cp * (tlo - t4b) + ql
 
    dpl = 100 * (tlo / t4b) ** k - 100
    vl = (spec_v(tlo, 100) + spec_v(t4a, 100)) / 2
-   wl = FANCURVE(vl * mdotHElow, dpl)  * 1 / 0.7457 # HP to kW
-   if mdotHElow < .2: return 9999
-   if wl < 0: return 99999
+   wl = FANCURVE(vl * mdotHElow / 1000, dpl)  * 1 / 0.7457 # HP to kW
+   print '---------===> ', t1a, t2a, t3a, t4a, w12, qh
+
+   if (
+   p3b / p4a > r or
+   qh > 2000 or
+   #mdotHEhigh < 0.2 or
+   LPd < 0 or HPd < 0 or 
+   LPd > 10. or
+   HPd > 1 or
+   tlo > t1a or
+   mdot < .2 or t4b < 0 or
+   p4b < 5 or
+   p4a < 5 or
+   p3a < 5 or
+   p3b < 5 or
+   p1b < 5 or
+   t3b < 0 or
+   t4a < 0 or
+   r< 1 or
+   mdotHElow < 0 or
+   wl < 0 or
+   p2b < 50 or
+   p2a < 0 or
+   t1b < 0 or
+   t2a < 0 or
+   #tlo < 200 or
+   p2b < 50 or 
+   t2b < 0): return 9999999
+   if w12 < 0: print 'w12<0: ', t2a, t1b, t1a, mdot, n_s_comp, cp ; quit()
+
+
    wnet = w12 + wh + wl - w34 # Wnet
    if wnet <0:  
+                print '---'
+                print qh
+                print '---'
+                print DV
                 print 'wnet<0 ', t2a, t1b, t3b, t4a
                 print p1b, p2a, p3b, p4a
-                print p4b, p1a, mdot, h_major(L_1, LPd, mdot) * gamma
+                print p4b, p1a, mdot, LPd, h_major(L_1, LPd, mdot) * gamma
                 print w12, w34 ; quit()
    if returnall: return [qh, t2b - t3a , ql, (t1a - t4b + t1a - tlo) / 2]
    #if returnall: print qh, wh, tho, t3a, mdotHEhigh ; quit() ; return [wh, (t2b - t3a + t3a - tho ) / 2, wl, (t4b - t1a + t1a - tlo) / 2]
@@ -137,22 +147,21 @@ def work(DV=[265., 1., 1., 1., 1., .25, 1., 2.], t3a=293.15, ql=100, t1a=275., c
 #plt.savefig('COP.pdf')
 
 # Optimization
-x0 = np.array([265., 1., 1., 1., 1., .25, 1., 15.])
+x0 = np.array([270., 1., 1., .5, 1., 15.])
 w0 = work(x0)
 from scipy.optimize import minimize
 con = {'type': 'ineq', 'fun': lambda x:100* np.min(x),} # non-negativity constraint
 x = minimize(work, x0, method="Powell", constraints=con, options={'maxiter':100000, 'disp':True, 'iprint':1}).x
+for _ in range(20): x = minimize(work, x, method="Powell", constraints=con, options={'maxiter':100000, 'disp':True, 'iprint':1}).x
 
 w = work(x)
 p = work(x, returnall=True)
 print 'tlo = ', x[0], ' K'
 print 'mdotHElow', x[1], 'kg/s'
 print 'mdot = ', x[2], ' kg/s'
-print 'LHEd = ', x[3], ' m'
-print 'HHEd = ', x[4], ' m'
-print 'HPd = ', x[5], ' m'
-print 'LPd = ', x[6], ' m'
-print 'pressure ratio is ', x[7]
+print 'HPd = ', x[3], ' m'
+print 'LPd = ', x[4], ' m'
+print 'pressure ratio is ', x[5]
 print 'Work is ', w, ' kW'
 print 'baseline is ', w0, ', kW'
 print ' '
